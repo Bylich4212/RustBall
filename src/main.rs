cadcfc7c98b89d76169da8f6ee0530cff16a749d
sebastian <checheko1500@gmail.com>
2025-04-14 18:06:25 -0400

    cubo que se mueve con awsd

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,61 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
-   let  x=5;
-   let  y=7;
-    let z=x+y;
-    print!("el resultado es{}",z);
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .add_systems(Update, player_movement)
+        .run();
+}
+
+#[derive(Component)]
+struct Player;
+
+fn setup(mut commands: Commands) {
+    // Cámara
+    commands.spawn(Camera2dBundle::default());
+
+    // Jugador
+    commands.spawn((
+        SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(0.3, 0.7, 0.9),
+                custom_size: Some(Vec2::new(50.0, 50.0)),
+                ..default()
+            },
+            transform: Transform::from_xyz(0.0, 0.0, 0.0),
+            ..default()
+        },
+        Player,
+    ));
+}
+
+fn player_movement(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut query: Query<&mut Transform, With<Player>>,
+    time: Res<Time>,
+) {
+    let speed = 200.0;
+
+    for mut transform in &mut query {
+        let mut direction = Vec3::ZERO;
+
+        if keyboard_input.pressed(KeyCode::KeyW) {
+            direction.y += 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::KeyS) {
+            direction.y -= 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::KeyA) {
+            direction.x -= 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::KeyD) {
+            direction.x += 1.0;
+        }
+
+        if direction.length() > 0.0 {
+            direction = direction.normalize();
+            transform.translation += direction * speed * time.delta_seconds();
+        }
+    }
 }
